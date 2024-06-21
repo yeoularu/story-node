e95c53bd05b9386b830c3ad12c3877aea6357834
오승태 <yeoularu@gmail.com>
2024-06-22 00:10:42 +0900

    feat: implement sticky post title and display content
    - Made the post title sticky at the top
    - Used Popover to show the full title when clicked
    - Added functionality to display the content of the post

--- app/(viewer_shell)/post/[folderId]/_components/SinglePost.tsx
+++ app/(viewer_shell)/post/[folderId]/_components/SinglePost.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Tables } from "@/lib/types/supabase";
 import { useQueryState } from "nuqs";
 import { useIntersectionObserver } from "usehooks-ts";
@@ -19,15 +24,34 @@ export default function SinglePost({
   const [_, setTitle] = useQueryState("title");
 
   const { ref } = useIntersectionObserver({
-    threshold: 0.8,
     onChange: (isIntersecting) => {
       if (isIntersecting) setTitle(post.title);
     },
   });
 
   return (
-    <div id={post.title ?? undefined} ref={ref} className="min-h-dvh">
-      <h1>{post.title}</h1>
+    <div id={post.id} ref={ref} className="mb-24">
+      <div className="sticky inset-x-0 top-1 z-40 mx-auto flex w-[calc(100%-3rem)] items-center justify-center text-center text-sm opacity-90">
+        <Popover>
+          <PopoverTrigger asChild>
+            <div className="mx-auto w-fit max-w-full rounded-md p-1.5 transition-colors hover:cursor-pointer hover:bg-transparent/5 hover:text-inherit">
+              <p
+                className={`line-clamp-1 whitespace-pre-wrap break-words text-center`}
+              >
+                {post.title}
+              </p>
+            </div>
+          </PopoverTrigger>
+          <PopoverContent>
+            <p className="whitespace-pre-wrap break-words text-center">
+              {post.title}
+            </p>
+          </PopoverContent>
+        </Popover>
+      </div>
+      <article className="whitespace-pre-wrap break-words bg-inherit px-0 sm:px-6">
+        {post.content}
+      </article>
     </div>
   );
 }
