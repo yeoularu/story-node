48bdbcf0be71e6aeee4f4e862de046980168d77d
오승태 <yeoularu@gmail.com>
2024-07-11 17:48:07 +0900

    feat: Add auto-save indicator to TypeWriter component
    - Added a Badge component to display auto-save status ("Saving..." and "Saved").
    - Updated state management to handle auto-save status updates during typing and erasing phases.
    - Adjusted typing and erasing speed configurations for better UX.
    - Improved code readability and maintainability with useRef and additional state variables.
    
    This update enhances the landing page editor animation by indicating the auto-save process, improving user feedback.

--- app/_components/landing-page/TypeWriter.tsx
+++ app/_components/landing-page/TypeWriter.tsx
@@ -1,30 +1,34 @@
+import { Badge } from "@/components/ui/badge";
 import React, { useEffect, useState } from "react";
 
 const TypeWriter = ({
   words = ["words"],
   writeSpeed = 80,
   eraseSpeed = 30,
-  pauseBetween = 1500,
+  pauseBetween = 2000,
   prefix = "prefix",
 }) => {
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [currentText, setCurrentText] = useState("");
   const [isDeleting, setIsDeleting] = useState(false);
   const [isPaused, setIsPaused] = useState(false);
+  const [autoSaveStateText, setAutoSaveStateText] = useState<
+    "" | "Saving..." | "Saved"
+  >("");
 
   useEffect(() => {
     const word = words[currentWordIndex];
 
     const getTypingSpeed = () => {
-      if (isDeleting && currentText.length === 0) return 1000;
-      if (isDeleting) return eraseSpeed;
       if (isPaused) return pauseBetween;
+      if (isDeleting) return eraseSpeed;
       return writeSpeed;
     };
 
     const timer = setTimeout(() => {
       if (!isDeleting && currentText.length < word.length) {
         setCurrentText(word.slice(0, currentText.length + 1));
+        setAutoSaveStateText("");
       } else if (
         !isDeleting &&
         currentText.length === word.length &&
@@ -32,18 +36,34 @@ const TypeWriter = ({
       ) {
         setIsPaused(true);
         setTimeout(() => {
+          setAutoSaveStateText("Saving...");
+        }, 500);
+        setTimeout(() => {
+          setAutoSaveStateText("Saved");
+        }, 1000);
+        setTimeout(() => {
           setIsPaused(false);
           setIsDeleting(true);
         }, pauseBetween);
       } else if (isDeleting && currentText.length > 0) {
         setCurrentText(word.slice(0, currentText.length - 1));
+        setAutoSaveStateText("");
       } else if (isDeleting && currentText.length === 0) {
+        setIsPaused(true);
+        setTimeout(() => {
+          setAutoSaveStateText("Saving...");
+        }, 500);
+        setTimeout(() => {
+          setIsPaused(false);
+        }, 900);
         setIsDeleting(false);
         setCurrentWordIndex((prevIndex) => (prevIndex + 1) % words.length);
       }
     }, getTypingSpeed());
 
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [
     currentText,
     currentWordIndex,
@@ -56,11 +76,20 @@ const TypeWriter = ({
   ]);
 
   return (
-    <p className="inline-block">
-      {prefix}
-      {currentText}
-      <span className="animate-blink -ml-1">│</span>
-    </p>
+    <div>
+      <Badge
+        className={`ml-auto block h-6 w-fit ${autoSaveStateText === "" ? "invisible" : "visible"}`}
+        variant="secondary"
+      >
+        {autoSaveStateText}
+      </Badge>
+
+      <p className="inline-block">
+        {prefix}
+        {currentText}
+        <span className="-ml-1 animate-blink">│</span>
+      </p>
+    </div>
   );
 };
 
